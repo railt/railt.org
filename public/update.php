9b0802e9554c2754726326a4ee258d0f9c65840b
Serafim <nesk@xakep.ru>
2017-10-08 23:59:24 +0300

    Update

---
+++ public/update.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * This file is part of railt.org package.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+declare(strict_types=1);
+
+
+\shell_exec('cd ../ && git pull origin master');
+\shell_exec('cd ../resources/docs && git pull  origin master');
