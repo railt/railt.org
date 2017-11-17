8cfef275ff047d4efd2e9a305f97a6a6f01ca386
Serafim <nesk@xakep.ru>
2017-11-17 06:25:22 +0300

    Improve exceptions (remove php trace)

--- app/Http/Controllers/TryOnlineController.php
+++ app/Http/Controllers/TryOnlineController.php
@@ -82,16 +82,7 @@ class TryOnlineController
                 $result[] = $definition;
             }
         } catch (SchemaException $e) {
-            $error = \str_replace([
-                \base_path('vendor/railt/railt'),
-                \base_path('vendor'),
-                \base_path('app'),
-                \base_path('public'),
-                \base_path(''),
-            ], '', (string)$e);
-
-            $error = \str_replace('):', '):' . "\n    ", $error);
-
+            $error = $e->getInfo() . "\n" . $compiler->getStack()->render();
             $trace = \collect($e->getCompilerTrace())
                 ->map(function(array $data) {
                     return [
